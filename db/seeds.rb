5c33986142bca3ac05ad0a09799c4bf1972dea35
Tului Gantulga <tgantulga@ucdavis.edu>
2021-05-18 13:42:13 -0700

    further progress, working on routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
+User.create(first_name: 'Tului', last_name: 'Gantulga', username: 'tgantulga', email: 'Tului@whyknot.me', password: '123456')
+User.create(first_name: 'TOny', last_name: 'King', username: 'tking', email: 'Tony@whyknot.me', password: '123456')
