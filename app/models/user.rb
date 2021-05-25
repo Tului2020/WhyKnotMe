3d0dc591a58bff6650314b29c3cc1e4a00d4297c
Tului Gantulga <tgantulga@ucdavis.edu>
2021-05-24 19:11:47 -0700

    added some constraints to signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, :session_token, presence: true, uniqueness: true
+  validates :username, :email, :session_token, presence: true, uniqueness: true
   validates :first_name, :last_name, presence: true
   validates :password_digest, presence: true
   validates :password, length: { minimum: 6, allow_nil: true}
